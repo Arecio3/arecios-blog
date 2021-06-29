d3290726f315e3e5d19cafc935f5526ca0775dce
Arecio Canton <cuba289@gmail.com>
2021-06-29 00:07:29 -0400

    initial commit

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+
+export default function NavBar() {
+    return (
+        <div>
+            <div className="top">
+                Navigation
+            </div>
+        </div>
+    )
+}
