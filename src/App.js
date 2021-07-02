97082d61df9aeb2e8f2174254378f3d479ceb30f
Arecio Canton <cuba289@gmail.com>
2021-07-02 16:24:15 -0400

    added react router and conditional rendering for non users

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react'
-import NavBar from './components/navBar/NavBar' 
-// import Home from './pages/home/Home'
-// import Single from './pages/single/Single'
-// import Write from './pages/write/Write';
-// import Settings from './pages/settings/Settings';
-// import Login from './pages/login/Login';
+import NavBar from './components/navBar/NavBar'
+import Home from './pages/home/Home'
+import Single from './pages/single/Single'
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './app.css'
 function App() {
+  const user = false;
   return (
-    <div className="App">
+    <Router className="App">
       <NavBar />
-      <Register />
-    </div>
+      <Switch>
+        <Route exact path='/'>
+            <Home />
+        </Route>
+        <Route path='/register'>
+            {user ? <Home /> : <Register />}
+        </Route>
+        <Route path='/login'>
+            {user ? <Home /> : <Login />}
+        </Route>
+        <Route path='/post/:postId'>
+            <Single />
+        </Route>
+        <Route path='/write'>
+           {user ? <Write /> : <Register />}
+        </Route>
+        <Route path='/settings'>
+            {user ? <Settings /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
