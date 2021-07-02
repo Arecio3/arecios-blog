db3149317efea86cb1b3f7f02e17374334b0bc5c
Arecio Canton <cuba289@gmail.com>
2021-07-02 15:51:25 -0400

    Styled and added login page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import NavBar from './components/navBar/NavBar'
 // import Home from './pages/home/Home'
 // import Single from './pages/single/Single'
 // import Write from './pages/write/Write';
-import Settings from './pages/settings/Settings';
+// import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
 import './app.css'
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Settings />
+      <Login />
     </div>
   );
 }
