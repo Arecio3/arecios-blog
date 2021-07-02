1e4c1bd75260066b67b33982945ef43180252667
Arecio Canton <cuba289@gmail.com>
2021-07-02 15:06:29 -0400

    styled settings page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import NavBar from './components/navBar/NavBar' 
 // import Home from './pages/home/Home'
 // import Single from './pages/single/Single'
-import Write from './pages/write/Write';
+// import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
 import './app.css'
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Write />
+      <Settings />
     </div>
   );
 }
