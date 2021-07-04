b0418ba920dda58cd2a0768b234faed42700a5fb
Arecio Canton <cuba289@gmail.com>
2021-07-04 18:45:47 -0400

    fixed reload issue and now successfully able to get user data on click

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './login.css'
+import React, { useRef, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import axios from 'axios'
+import "./login.css";
 
 export default function Login() {
-    return (
-        <div className='login'>
-            <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input className='loginInput' type="text" placeholder='example@gmail.com'/>
-                <label>Password</label>
-                <input className='loginInput' type="password" placeholder='Enter your password'/>
-                <button className="loginButton">Login</button>
-            </form>
-            <button className='loginRegisterButton'>
-            <Link className='link' to='/register'>Register</Link>
-            </button>
-        </div>
-    )
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({type:'LOGIN_START'})
+    try {
+        const res = await axios.post('/auth/login', {
+            username: userRef.current.value,
+            password: passwordRef.current.value,
+        })
+        dispatch({type:'LOGIN_SUCCESS', payload:res.data })
+
+    } catch(err) {
+        dispatch({type:'LOGIN_FAILURE'})
+    }
+  };
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input className="loginInput" type="text" placeholder="username" ref={userRef}/>
+        <label>Password</label>
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter your password"
+          ref={passwordRef}
+        />
+        <button className="loginButton" type="submit" disabled={isFetching}>
+          Login
+        </button>
+      </form>
+      <button className="loginRegisterButton">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+      </button>
+    </div>
+  );
 }
