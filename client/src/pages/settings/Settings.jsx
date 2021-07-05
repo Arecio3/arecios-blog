26b07b96fc7a11de804a8f33fedc8d82a9e5f472
Arecio Canton <cuba289@gmail.com>
2021-07-04 23:16:21 -0400

    added delete account function but not finished

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import Sidebar from "../../components/sidebar/SideBar";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./settings.css";
 
@@ -9,14 +10,14 @@ export default function Settings() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [success, setSuccess] = useState(false)
-  
+  const [success, setSuccess] = useState(false);
+
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "http://localhost:5000/images/";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch({type: 'UPDATE_START'})
+    dispatch({ type: "UPDATE_START" });
     const updatedUser = {
       userId: user._id,
       username,
@@ -38,11 +39,22 @@ export default function Settings() {
     }
     try {
       const res = await axios.put("/users/" + user._id, updatedUser);
-      setSuccess(true)
-      dispatch({type: 'UPDATE_SUCCESS', payload: res.data})
+      setSuccess(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
     } catch (err) {
       console.error(err);
-      dispatch({type:'UPDATE_FAILURE'})
+      dispatch({ type: "UPDATE_FAILURE" });
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/users/${user._id}`, {
+        data: { userId: user._id },
+      });
+      window.location.replace("/register");
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -51,12 +63,17 @@ export default function Settings() {
       <div className="settingsWrapper">
         <div className="settingsTitle">
           <span className="settingsUpdateTitle">Update Account</span>
-          <span className="settingsDeleteTitle">Delete Account</span>
+            <button className="settingsDeleteTitle" onClick={handleDelete}>
+            <Link className='link' to="/login">Delete Account</Link>
+            </button>
         </div>
         <form className="settingsForm" onSubmit={handleSubmit}>
           <label>Profile Picture</label>
           <div className="settingsProfilePic">
-            <img src={file ? URL.createObjectURL(file) : PF + user.profilePic} alt="" />
+            <img
+              src={file ? URL.createObjectURL(file) : PF + user.profilePic}
+              alt=""
+            />
             <label htmlFor="fileInput">
               <i className="settingsPPIcon far fa-user-circle"></i>
             </label>
@@ -68,13 +85,33 @@ export default function Settings() {
             />
           </div>
           <label>Username</label>
-          <input type="text" placeholder={user.username} onChange={e => setUsername(e.target.value)}/>
+          <input
+            type="text"
+            placeholder={user.username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <label>Email</label>
-          <input type="email" placeholder={user.email} onChange={e => setEmail(e.target.value)}/>
+          <input
+            type="email"
+            placeholder={user.email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Password</label>
-          <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
-          <button className="settingsSubmit" type='submit'>Update</button>
-          {success && <span style={{color:'green', textAlign: 'center', marginTop: 20}}>Profile was updated!</span>}
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="settingsSubmit" type="submit">
+            Update
+          </button>
+          {success && (
+            <span
+              style={{ color: "green", textAlign: "center", marginTop: 20 }}
+            >
+              Profile was updated!
+            </span>
+          )}
         </form>
       </div>
       <Sidebar />
