fc07dcecb9ba521d911be0101e6259b650020099
Arecio Canton <cuba289@gmail.com>
2021-07-04 20:30:28 -0400

    added delete functionality

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { Context } from '../../context/Context'
 import { useLocation, Link } from "react-router-dom";
 import Nature from "../../images/natureBlogPic.jpeg";
 import axios from "axios";
@@ -11,7 +12,21 @@ export default function SinglePost() {
   // console.log(location.pathname.split('/')[2]) 
   const path = (location.pathname.split('/')[2])
   const [post, setPost] = useState({})
+  const { user } = useContext(Context);
+  const PF = 'http://localhost:5000/images/'
   // So we make an axios call to get the data that was clicked on by the id "path"
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${post._id}`, {
+        data:{username: user.username}
+      });
+      window.location.replace("/")
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get('/posts/' + path)
@@ -23,13 +38,15 @@ export default function SinglePost() {
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        {post.photo && ( <img className="singlePostImg" src={post.photo} alt="" />)}
+        {post.photo && ( <img className="singlePostImg" src={PF + post.photo} alt="" />)}
         <h1 className="singlePostTitle">
          {post.title}
+         {post.username === user?.username && (
           <div className="singlePostEdit">
             <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
+            <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
           </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
